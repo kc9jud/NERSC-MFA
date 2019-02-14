1e2c2d4b33e818b90a7a51b67d7a5177f96bdebc
abe <abe@DOE6748468-3.local>
2019-02-14 13:03:29 -0800

    Added version change

--- sshproxy.sh
+++ sshproxy.sh
@@ -25,7 +25,7 @@
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 progname=$(basename $0)
-version="1.0.4"
+version="1.1.0"
 
 # Save tty state for trap function below
 original_tty_state=$(stty -g)
