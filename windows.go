5ff869e0bd832ac2168522f6cb504d5c16a770e5
Matt Dunford <mdunford@lbl.gov>
2019-02-27 14:15:23 -0800

    try out a windows binary

---
+++ windows.go
@@ -0,0 +1,125 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"io/ioutil"
+	"net/http"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/urfave/cli"
+	"golang.org/x/crypto/ssh/terminal"
+)
+
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "sshproxy"
+	app.Usage = "sshproxy grabs keys from NERSC"
+	app.Before = func(ctx *cli.Context) error {
+		if ctx.GlobalBool("debug") {
+			logrus.SetLevel(logrus.DebugLevel)
+		}
+		return nil
+	}
+	app.Version = "1.0"
+	app.Author = "Matt Dunford"
+	app.Email = "mdunford@lbl.gov"
+	app.Action = grabPpk
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "debug, d",
+			Usage: "Debug logging",
+		},
+		cli.StringFlag{
+			Name:  "user, u",
+			Usage: "user to authenticate as",
+		},
+		cli.StringFlag{
+			Name:  "output, o",
+			Usage: "key file name",
+			Value: "nersckey.ppk",
+		},
+		cli.StringFlag{
+			Name:  "scope, s",
+			Usage: "key scope",
+			Value: "default",
+		},
+		cli.StringFlag{
+			Name:  "server, U",
+			Usage: "server to grab keys from",
+			Value: "sshproxy.nersc.gov",
+		},
+	}
+	app.Run(os.Args)
+}
+
+func promptPassword() (password string, err error) {
+	fmt.Print("Password: ")
+	bytes, err := terminal.ReadPassword(0)
+	password = string(bytes)
+	return
+}
+
+func grabPpk(ctx *cli.Context) error {
+
+	username := ctx.GlobalString("user")
+	if len(username) < 1 {
+		logrus.Fatal("-u username is required")
+	}
+
+	fname := ctx.GlobalString("output")
+	if len(fname) < 1 {
+		logrus.Fatal("no file name specified")
+	}
+
+	scope := ctx.GlobalString("scope")
+	if len(scope) < 1 {
+		logrus.Fatal("no scope specified")
+	}
+
+	server := ctx.GlobalString("server")
+	if len(server) < 1 {
+		logrus.Fatal("no scope specified")
+	}
+
+	password, err := promptPassword()
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	url := fmt.Sprintf("https://%s/create_pair/%s/?putty", server, scope)
+	client := &http.Client{}
+	req, err := http.NewRequest("POST", url, nil)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	req.SetBasicAuth(username, password)
+
+	logrus.Debugf("contacting %s", url)
+	resp, err := client.Do(req)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		logrus.Fatalf("%s returned status code %d: %s", url, resp.StatusCode, resp.Status)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+
+	f, err := os.Create(fname)
+	if err != nil {
+		logrus.Fatal(err)
+	}
+	defer f.Close()
+	w := bufio.NewWriter(f)
+	w.Write(body)
+	w.Flush()
+	return nil
+}
